a277176eca32f79d815e57ff4fd576dc61fb88b9
Uwe Stuehler <ustuehler@growit.io>
2017-12-08 23:23:08 +0100

    test(config): update tests for new feature

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,16 +3,34 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
+function generator(config) {
+  return helpers.run(path.join(__dirname, '../generators/app')).withPrompts(config);
+}
+
 describe('generator-gcloud:app', () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+  describe('with cloudbuild:true', () => {
+    beforeAll(() => {
+      return generator({ cloudbuild: true });
+    });
+
+    it('creates the expected files', () => {
+      assert.file(['.yo-rc.json']);
+      assert.file(['package.json']);
+      assert.file(['config.yaml']);
+      assert.file(['cloudbuild.yaml']);
+    });
   });
 
-  it('creates files', () => {
-    assert.file(['package.json']);
-    assert.file(['config.yaml']);
-    assert.file(['cloudbuild.yaml']);
+  describe('with cloudbuild:false', () => {
+    beforeAll(() => {
+      return generator({ cloudbuild: false });
+    });
+
+    it('creates the expected files', () => {
+      assert.file(['.yo-rc.json']);
+      assert.file(['package.json']);
+      assert.file(['config.yaml']);
+      assert.noFile(['cloudbuild.yaml']);
+    });
   });
 });
